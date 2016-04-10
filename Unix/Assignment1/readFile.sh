5fb5e4a9b8d637f271bcec610c78f41efa94c3c1
praninder <praninder.vuppala@outlook.com>
2016-04-10 23:46:25 +0000

    Unix Assignment 1

--- Unix/Assignment1/readFile.sh
+++ Unix/Assignment1/readFile.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 fileName=$1
-echo -e "\e[1mAnswer1"
+echo -e "\e[5mAnswer 1a & 1b"
 echo "Number of words";wc -w $fileName 
 echo "Number of lines";wc -l $fileName
+echo -e "\e[5mAnswer 1c"
 awk '{print $2}' $fileName
+echo -e "\e[5mAnswer1d"
 sed -i.bak -e s/raj/ram/gI $fileName                                                                                                                          
 cat $fileName                                                                                                                                                 
 mv employee.bak employee
