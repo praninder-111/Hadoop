d2e2cf36f35b22537252b23b18bdd49271c0f191
praneender <praninder.vuppala@outlook.com>
2016-04-10 20:32:29 -0500

    Unix Assignment1

--- Unix/Assignment1/fileSize.sh
+++ Unix/Assignment1/fileSize.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-echo -e "Answer 2a"
 ls -l test/assign2/|awk '{print $5" "$6" "$7}' > test/assign2/output.txt
+if [$?]
+echo "File outputfile generated successfully"
 echo -e "Answer 2b"
 
 
